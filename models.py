b81da53a9713ac01e9955fccab88fcc1f0717abc
Iman Mirzadeh <iman.kinder@gmail.com>
2020-03-30 23:13:32 -0700

    Remove batchnorms again

--- models.py
+++ models.py
@@ -69,21 +69,21 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(in_planes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
+        # self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
+        # self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1,
                           stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
+                #nn.BatchNorm2d(self.expansion * planes)
             )
 
     def forward(self, x):
-        out = relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        out = relu(self.conv1(x))#relu(self.bn1(self.conv1(x)))
+        out = self.conv2(out)#self.bn2(self.conv2(out))
         out += self.shortcut(x)
         out = relu(out)
         return out
@@ -95,7 +95,7 @@ class ResNet(nn.Module):
         self.in_planes = nf
 
         self.conv1 = conv3x3(3, nf * 1)
-        self.bn1 = nn.BatchNorm2d(nf * 1)
+        # self.bn1 = nn.BatchNorm2d(nf * 1)
         self.layer1 = self._make_layer(block, nf * 1, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, nf * 2, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, nf * 4, num_blocks[2], stride=2)
@@ -112,7 +112,7 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         bsz = x.size(0)
-        out = relu(self.bn1(self.conv1(x.view(bsz, 3, 32, 32))))
+        out = relu(self.conv1(x.view(bsz, 3, 32, 32)))#relu(self.bn1(self.conv1(x.view(bsz, 3, 32, 32))))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
