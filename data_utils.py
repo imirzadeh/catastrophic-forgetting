d552cdfae813d4b2cb82c50d4d47118e56ab46c5
Iman Mirzadeh <iman.kinder@gmail.com>
2020-03-30 20:41:04 -0700

    Debug split cifar - original dataset

--- data_utils.py
+++ data_utils.py
@@ -98,13 +98,29 @@ def get_split_cifar100(task_id, batch_size=BATCH_SIZE, shuffle=False):
 	return train_loader, test_loader
 
 
-def get_split_cifar100_tasks(num_tasks, shuffle=False, batch_size=BATCH_SIZE):
+def get_split_cifar100_tasks_2(num_tasks, shuffle=False, batch_size=BATCH_SIZE):
 	datasets = {}
 	for task_id in range(1, num_tasks):
 		train_loader, test_loader = get_split_cifar100(task_id, batch_size, shuffle)
 		datasets[task_id] = {'train': train_loader, 'test': test_loader}
 	return datasets
 
+
+def get_split_cifar100_tasks(num_tasks, shuffle=False, batch_size=BATCH_SIZE):
+	transforms = torchvision.transforms.Compose([
+		torchvision.transforms.ToTensor(),
+		torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+		])
+
+	train = torchvision.datasets.CIFAR100('./data/', train=True, download=True, transform=transforms)
+	test = torchvision.datasets.CIFAR100('./data/', train=False, download=True, transform=transforms)
+	train_loader = torch.utils.data.DataLoader(train, pin_memory=True)
+	test_loader = torch.utils.data.DataLoader(test, pin_memory=True)
+
+	datasets = {}
+	for task_id in range(1, num_tasks):
+		datasets[task_id] = {'train': train_loader, 'test': test_loader}
+	return datasets
 
 if __name__ == "__main__":
 	dataset = get_split_cifar100_tasks()
