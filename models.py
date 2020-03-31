51f6978859a00a42d7f6b17646e93513af3559ae
Iman Mirzadeh <iman.kinder@gmail.com>
2020-03-30 23:50:37 -0700

    Reduce batchsize to 10

--- models.py
+++ models.py
@@ -79,7 +79,7 @@ class BasicBlock(nn.Module):
                 nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1,
                           stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion * planes),
-                nn.Dropout(0.1),
+                # nn.Dropout(0.05),
             )
 
     def forward(self, x):
