a78d4f0e243c1e29fb6252111ff0145adc7885f9
Iman Mirzadeh <iman.kinder@gmail.com>
2020-03-31 02:09:47 -0700

    Add IC Layer

--- models.py
+++ models.py
@@ -143,23 +143,32 @@ class BasicBlock(nn.Module):
 	def __init__(self, in_planes, planes, stride=1):
 		super(BasicBlock, self).__init__()
 		self.conv1 = conv3x3(in_planes, planes, stride)
-		self.bn1 = nn.BatchNorm2d(planes)
+		# self.bn1 = nn.BatchNorm2d(planes)
 		self.conv2 = conv3x3(planes, planes)
-		self.bn2 = nn.BatchNorm2d(planes)
+		# self.bn2 = nn.BatchNorm2d(planes)
 
 		self.shortcut = nn.Sequential()
 		if stride != 1 or in_planes != self.expansion * planes:
 			self.shortcut = nn.Sequential(
-				nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1,
-						  stride=stride, bias=False),
-				nn.BatchNorm2d(self.expansion * planes)
+				nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+				# nn.BatchNorm2d(self.expansion * planes)
+			)
+		self.IC = nn.Sequential(
+			nn.BatchNorm2d(self.expansion * planes),
+			nn.Dropout(p=0.05)
 			)
 
 	def forward(self, x):
-		out = relu(self.bn1(self.conv1(x)))
-		out = self.bn2(self.conv2(out))
+		out = self.conv1(x)
+		out = relu(out)
+		out = self.IC(out)
+		out = self.conv2(x)
+
+		# IC Layer
+
 		out += self.shortcut(x)
 		out = relu(out)
+		out = self.IC(out)
 		return out
 
 
