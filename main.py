84c4d926f207e325549f38eb9f419ac8da3b539e
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-02 20:38:59 -0700

    Update config

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ from data_utils import get_permuted_mnist_tasks, get_rotated_mnist_tasks, get_sp
 first_stable = True if np.random.randint(2) == 1 else False
 second_stable = True if np.random.randint(2) == 1 else False
 
-config_stable   = {'lr': 0.15,  'gamma': 0.3, 'momentum': 0.8, 'lrlb': 0.0001, 'dropout': 0.25, 'batch_size': 32, 'epochs': 5}
-config_unstable = {'lr': 0.10, 'gamma': 1.0, 'momentum': 0.8, 'lrlb': 0.0001, 'dropout': 0.0, 'batch_size': 256, 'epochs': 5}
+config_stable   = {'lr': 0.1,  'gamma': 0.3, 'momentum': 0.8, 'lrlb': 0.0001, 'dropout': 0.25, 'batch_size': 32, 'epochs': 5}
+config_unstable = {'lr': 0.05, 'gamma': 1.0, 'momentum': 0.8, 'lrlb': 0.0001, 'dropout': 0.0, 'batch_size': 256, 'epochs': 5}
 
 
 
@@ -191,7 +191,7 @@ def run():
 		# task_lr = config['lr']*(config['gamma']**(current_task_id-1))
 		for epoch in range(1, EPOCHS+1):
 			# train and save
-			lr = max(config['lr']* config['gamma']**(epoch), config['lrlb'])
+			lr = max(config['lr']* config['gamma']**(current_task_id), config['lrlb'])
 			print(lr)
 
 			optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=config['momentum'])
